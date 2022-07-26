b8d2443105ba7cfce2b3efb872dff59a22284325
udeaghad <dozie.udeagha@gmail.com>
2022-07-26 15:20:33 +0100

    Fetch data and create reducer for mission

---
+++ src/redux/missions/missionReducer.js
@@ -0,0 +1,21 @@
+const mission = [];
+
+export default (state = mission, action) => {
+  switch (action.type) {
+    case 'GET_ALL_MISSION/fulfilled':
+      return [...state, action.payload];
+    
+    case 'JOIN_MISSION':
+     state.map(item => {
+      if(item.mission_id === action.payload){
+        return {
+          ...state,
+          join_mission: !state.join_mission,
+        };
+      }
+     })
+
+     default:
+      return state
+  }
+}
