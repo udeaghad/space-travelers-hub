8b7828dbb22a5a885174308e30ae762fdbebef8a
Fissha <yonas.fissha@ailylabs.com>
2022-07-26 05:24:30 +0100

    Create navbar for the website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar.js' 
 
 const App = () => (
   <div className="App">
     <div>Space Travelers Project</div>
+    <NavBar />
   </div>
 );
 
