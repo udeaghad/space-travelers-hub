c318e286d03121c3b1f386f6fa4e24b5d5505938
udeaghad <dozie.udeagha@gmail.com>
2022-07-26 18:51:26 +0100

    render mission API items

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,4 +1,30 @@
+import {useDispatch, useSelector} from 'react-redux'
+import React, { useEffect } from 'react';
+import { getMissions } from '../redux/missions/missionAction';
+
 const Mission = () => {
+
+const dispatch = useDispatch()
+
+useEffect(() => {
+  dispatch(getMissions());
+}, []);
+
+const showMission = useSelector(state => state.Mission)
+return (
+  <div >      
+      {showMission.map(mission => ( 
+                
+        <ul className='wrapper' key={mission.mission_id}>
+         <li className='name-cell'>{mission.mission_name}</li>
+         <li className='description-cell'>{mission.description}</li>  
+               
+        </ul>
+      
+      ))}
+   
+</div>
+)
 
 }
 
--- src/redux/missions/missionReducer.js
+++ src/redux/missions/missionReducer.js
@@ -1,14 +1,10 @@
-import {useDispatch} from 'react-redux'
-import { useEffect } from 'react';
-import { getMissions } from './missionAction';
-
 const mission = [];
 
 export default (state = mission, action) => {
   switch (action.type) {
     case 'GET_ALL_MISSION/fulfilled':
       
-      return [...state, action.payload];
+      return action.payload;
     
     case 'JOIN_MISSION':
      state.map(item => {
@@ -25,8 +21,3 @@ export default (state = mission, action) => {
   }
 }
 
-const dispatch = useDispatch()
-
-useEffect(() => {
-  dispatch(getMissions());
-}, []);
