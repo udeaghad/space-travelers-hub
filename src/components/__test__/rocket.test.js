e2946574ec6fd940f9f3765aa0e36dea8490ab6d
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-27 12:45:47 +0300

    Add test for rocket.js

---
+++ src/components/__test__/rocket.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import Rocket from "../Rocket";
+import store from "../../redux/configureStore";
+import rocketReducer from "../../redux/rockets/rockets";
+import { fetchRocketsFromApi } from "../../redux/rockets/rockets";
+
+describe('Jest test' , () => {
+    it('Match snapshot' , () => {
+        const { container } = render(
+            <MemoryRouter>
+                <Provider store={store}>
+                    <Rocket />
+                </Provider>
+            </MemoryRouter>,
+        );
+        expect(container.firstChild).toMatchSnapshot();
+    })
+
+
+    it('Output should be empty state' , () => {
+        const state = rocketReducer(undefined , {});
+        expect(state).toEqual([])
+    })
+
+
+    it('should return content from api' , () => {
+        const result = fetchRocketsFromApi();
+        expect(result).not.toEqual(null);
+    })
+})
