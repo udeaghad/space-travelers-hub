98bfc289d3b013209d9306976861e07690ea46e5
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-27 08:55:14 +0300

    Add reserved rockets to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,17 @@
 import {useSelector} from 'react-redux'
+import '../styles/profile.css'
 
 
 
 const Profile = () => {
 
   const showMission = useSelector(state => state.Mission)
-
+  const rockets = useSelector(state => state.Rockets)
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved)
   const missionJoined = showMission.filter(mission => mission.join_mission === true)
   
-
   return (
-    <div>
+    <div className='profile-page'>
       <div className="mission-profile">
         <h2>My Missions</h2>
         <ul>
@@ -18,8 +19,17 @@ const Profile = () => {
           <li key={mission.mission_id}>{mission.mission_name}</li>
         ))}
         </ul>
-        
 
+      </div>
+      <div className='reserved-rockets'>
+      <h2>My Rockets</h2>
+      {reservedRockets.length ? (
+        <div>
+          {reservedRockets.map((rocket) => (
+            <p className='reserved-rocket' key={rocket.id}>{rocket.name}</p>
+          ))}
+        </div>
+      ) : null}
       </div>
     </div>
   )
