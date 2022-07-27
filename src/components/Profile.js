824afe7dbb6f2f1f8a661423751d639e1b170638
udeaghad <dozie.udeagha@gmail.com>
2022-07-27 11:38:25 +0100

    Implement the profile for mission

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,25 @@
+import {useSelector} from 'react-redux'
+
+
+
 const Profile = () => {
+
+  const showMission = useSelector(state => state.Mission)
+
+  const missionJoined = showMission.filter(mission => mission.join_mission === true)
+  
+
+  return (
+    <div>
+      <div className="mission-profile">
+        <h2>My Missions</h2>
+        {missionJoined.map(mission => ( 
+          <div key={mission.mission_id}>{mission.mission_name}</div>
+        ))}
+
+      </div>
+    </div>
+  )
 
 }
 
